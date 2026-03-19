36825009e85b8dc6e8fa5b653961df28453e5913
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2026-03-18 22:00:31 -0400

    Open API swagger integration done

--- transaction-service/src/main/java/com/banking/transactionservice/controller/TransactionController.java
+++ transaction-service/src/main/java/com/banking/transactionservice/controller/TransactionController.java
@@ -2,14 +2,16 @@ package com.banking.transactionservice.controller;
 
 import com.banking.transactionservice.dto.TransferRequest;
 import com.banking.transactionservice.service.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/transactions")
 @RequiredArgsConstructor
@@ -17,6 +19,7 @@ public class TransactionController {
 
     private final TransactionService transactionService;
 
+    @Operation(summary = "Transfer money between accounts")
     @PostMapping("/transfer")
     public ResponseEntity<Void> transfer(@Valid @RequestBody TransferRequest request,
                                          @AuthenticationPrincipal Jwt jwt) {
